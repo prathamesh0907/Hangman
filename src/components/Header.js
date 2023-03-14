285e91593c0ef584b3faed380bd58d56331dc075
Prathamesh Sawant <prathameshvs2000@gmail.com>
2023-03-14 14:56:35 +0530

    Initial Commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <>
+        <h1>Hangman</h1>
+        <p>Find the Hidden Word - Enter a Letter</p>
+    </>
+  )
+}
+
+export default Header
